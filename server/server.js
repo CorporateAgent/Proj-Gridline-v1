85154a3561dc91a7387c2335a8d562aa72b74591
CorporateAgent <lorenzop@tfg.co.za>
2024-08-26 21:03:01 +0200

    v0.2.1

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
-const fetch = require('node-fetch'); // Using static import of fetch
 
 const app = express();
 const PORT = process.env.PORT || 3000;
